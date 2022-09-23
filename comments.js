a8d93011b7e4c4ad082c4eba7b456a78694ffe13
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-09-23 18:45:32 -0300

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Write comment
+console.log('Introducao a funcao "comentários"')// Log que descreve o objetivo do arquivo
+
+/*
+Bloco de comentários
+em linhas consecutivas
+*/
+
+/*************** organizador de secão ***************/
+
+console.log('Olá')
+//console.log(',') -> Transformado em comentário para impossibilitar sua ida para o console
+console.log('Mundo!')
---
+++ console.js
@@ -0,0 +1,3 @@
+console.log('Meu nome é "Patrício". Estou aprendendo JavaScript às',10,'da noite');
+
+alert('Alert!')
