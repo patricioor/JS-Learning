0186500f40d6f6e95ab5aba9a6144a65375753b0
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-09-23 19:32:35 -0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,24 @@
+let name; // Variable declared
+name = 'Patrício'; // Incrementing the variable 
+
+console.log(name,'was born in 1994')
+console.log(name,'met Carol in 2020 ')
+console.log('At 2022, ',name,' marry with Carol')
+console.log('Carol had one child with ',name,' at 2024')
+console.log("Name of ",name,"'s son is Artur")
+
+name = 'Tiço'; // New value to the variable
+
+console.log(name,'was born in 1994')
+console.log(name,'met Carol in 2020 ')
+console.log('At 2022, ',name,' marry with Carol')
+console.log('Carol had one child with ',name,' at 2024')
+console.log("Name of ",name,"'s son is Artur")
+
+//I can't create variables with reserved words
+// Variables must have meaningful names
+// I cannot start a variable name with a number
+// Cannot contain spaces or dashes in the variable
+// CamelCase is used to represent the beginning of a new word. Ex: nameClient
+// Case-sensitive. Ex: nameClient ≠ nameclient
+// Cannot recreate an already existing variable
