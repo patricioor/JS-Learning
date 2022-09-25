95540b1a34a4663789f479db87a36db676936119
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-09-24 21:00:31 -0300

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,8 @@
+// const name = 'Patrício';
+// console.log(name)
+//const name = 'Tisso' -> can't change value predefined of constant
+
+const firstNumb = 5;
+const secondNumb = 10;
+const calc = firstNumb * secondNumb;
+console.log(calc)
