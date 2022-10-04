7766f1a7571c78f00ce704d76174a94acd943a59
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-10-04 11:44:02 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+const potent = n => n ** 0.5; // Can be used when have only one parameter
+
+console.log(potent(25))
+console.log(potent(121))
+console.log(potent(169))
+
+// or
+
+// function add(x,y) {
+//   const result = x + y;
+//   return result; // nothing after the 'return' will be taken into account
+//   console.log('Hi') // will not be displayed
+// }
+
+
+// const result = add(3,2)
+// console.log(result) // console -> 5.
+
+
+// // function any1(name){
+// //   return `Hello, ${name}`;
+// // }
+
+// // const any = any1('Patrício');
+// // console.log(any) // console -> Hello, Patrício
