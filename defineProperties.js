c82e60dad7369f2477193d7dc6f356eb1949346e
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2023-01-11 17:56:27 -0300

    Add files via upload

---
+++ defineProperties.js
@@ -0,0 +1,40 @@
+// defineProperty - defineProperties
+function Produto(nome, preco, estoque) {
+  let nomePro = nome;
+  let precoPro = preco;
+  let estoquePro = estoque;
+
+  
+  Object.defineProperties(this, {
+    nome: {
+      enumerable: true, // mostra a chave
+      configurable: true, // Se pode apagar ou reconfigurar
+      get: () => nomePro,
+      set: (valor) => {
+        if(typeof valor !== 'string') throw new TypeError('Erooooow! Tem que ser uma string!!');
+        nomePro = valor;
+      }
+    },
+    preco: {
+      enumerable: true, // mostra a chave
+      configurable: false, // Se pode apagar ou reconfigurar
+      get: () => precoPro,
+      set: (valor) => {
+        if (typeof valor !== 'number') throw new TypeError('Eroooow! Tem que ser um number!!');
+        precoPro = valor;
+      }
+    },
+    estoque: {
+      enumerable: true,
+      configurable: true,
+      get: () => estoquePro,
+      set: (valor) => {
+        if (typeof valor !== 'number') throw new TypeError('Eroooow! Tem que ser um number!!');
+        estoquePro = valor;
+      }
+    }
+  })
+}
+const descricaoCompleta = () => `Produto: ${p1.nome}, Preço: R$${p1.preco},00, Estoque: ${p1.estoque}`
+const p1 = new Produto('Camiseta', 100, 3);
+console.log(Object.getOwnPropertyDescriptors(p1))
